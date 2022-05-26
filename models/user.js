0d253147225739df73f3ed4d659919e1f18cbf7b
Rohan Verma <rohanvermaara@gmail.com>
2022-05-26 19:28:06 +0530

    added users model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'core'
+    }
+})
+
+const user = mongoose.model('user', userSchema)
+module.exports = user
